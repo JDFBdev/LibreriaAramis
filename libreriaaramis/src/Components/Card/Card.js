4bba8fa0ef8d5f079ff0db1839f13e9da806e891
J. Diego Fernández <JDFBdeveloper@gmail.com>
2022-06-10 15:11:50 -0300

    Fake cards

--- libreriaaramis/src/Components/Card/Card.js
+++ libreriaaramis/src/Components/Card/Card.js
@@ -10,6 +10,8 @@ export default function Card({product, disableCart}){
 
     const handleCarrito = function(){
 
+        console.log(disableCart)
+
         if (!disableCart){
             let productsCart = [];
 
