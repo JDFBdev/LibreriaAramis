a1ea8908f76f9deba39485b04cf64526885e8f70
J. Diego Fernández <JDFBdeveloper@gmail.com>
2022-07-18 15:17:34 -0300

    Admin no accesible

--- libreriaaramis/src/App.js
+++ libreriaaramis/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Home from './Components/Home/Home';
 import Search from './Components/Search/Search';
-import Admin from './Components/Admin/Admin';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import NotFound from './Components/NotFound/NotFound';
@@ -14,7 +13,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/Search/:param" element={<Search />}/>
-          <Route path="/kYWQ8vVKstxY8C2n" element={<Admin />}/>
           <Route path="/*" element={<NotFound/>} />
         </Routes>
       </BrowserRouter>
