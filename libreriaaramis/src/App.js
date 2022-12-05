c84d7719d98884d56dbe102cfab86876adf35e87
J. Diego Fernández <JDFBdeveloper@gmail.com>
2022-12-05 13:08:23 -0300

    Mantenimiento

--- libreriaaramis/src/App.js
+++ libreriaaramis/src/App.js
@@ -4,18 +4,20 @@ import Search from './Components/Search/Search';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import NotFound from './Components/NotFound/NotFound';
+import Mantenimiento from './Components/Mantenimiento/Mantenimiento';
 
 function App() {
   return (
     <>
       <Toaster/>
-      <BrowserRouter>
+      <Mantenimiento/>
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/Search/:param" element={<Search />}/>
           <Route path="/*" element={<NotFound/>} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
       </>
   );
 }
